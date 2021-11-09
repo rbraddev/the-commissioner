58c2f2ac5ebe2b6405fd66b2a2d4004c5a461c4e
Ryan Bradshaw <ryan@rbradshaw.dev>
2021-11-09 16:19:20 +0000

    added site inventory endpoint

--- backend/app/api/inventory.py
+++ backend/app/api/inventory.py
@@ -1,11 +1,12 @@
 from operator import or_
 from typing import *
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy.future import select
 from sqlalchemy import or_
 
 from app.core.inventory.tasks import update_inventory
+from app.core.security.utils import get_current_user
 from app.db import get_session
 from app.models.inventory import (
     NetworkRead,
@@ -14,12 +15,13 @@ from app.models.inventory import (
     DesktopRead,
     SearchResults,
 )
+from app.models.token import User
 
 router = APIRouter()
 
 
 @router.post("/update/network", status_code=201)
-async def start_update_task():
+async def start_network_update_task(current_user: User = Depends(get_current_user)):
     """
     Update network inventory DB from SW
     """
@@ -28,7 +30,8 @@ async def start_update_task():
 
 
 @router.post("/update/desktop", status_code=201)
-async def start_update_task():
+async def start_desktop_update_task(current_user: User = Depends(get_current_user)
+):
     """
     Update desktop inventiry DB from SW
     """
@@ -38,10 +41,12 @@ async def start_update_task():
 
 @router.get("/network", response_model=List[NetworkRead], status_code=200)
 async def get_network_devices(
-    # session: AsyncSession = Depends(get_session),
     site: str = None,
     device_type: str = None,
-):
+):  
+    """
+    Filter network devices
+    """
     expression = None
     if site is None and device_type is None:
         expression = select(Network)
@@ -65,9 +70,11 @@ async def get_network_devices(
 
 @router.get("/desktop", response_model=List[DesktopRead], status_code=200)
 async def get_desktop_devices(
-    # session: AsyncSession = Depends(get_session),
     site: str = None,
 ):
+    """
+    Filter desktops
+    """
     expression = None
     if site is None:
         expression = select(Desktop)
@@ -80,8 +87,35 @@ async def get_desktop_devices(
     return devices
 
 
+@router.get("/site", response_model=List[SearchResults], status_code=200)
+async def get_site_devices(
+    site: str = None,
+):
+    """
+    Get site inventory
+    """
+    if site is None:
+        return []
+    
+    results = {"desktops": [], "network_devices": []}
+    
+    async with get_session() as session:
+        desktop_results = await session.execute(select(Desktop).where(Desktop.site == site))
+        desktops = desktop_results.scalars().all()
+        results.update({"desktops": desktops})
+
+        network_device_results = await session.execute(select(Network).where(Network.site == site))
+        network_devices = network_device_results.scalars().all()
+        results.update({"network_devices": network_devices})
+    
+    return [results]
+
+
 @router.get("/search", response_model=List[SearchResults], status_code=200)
 async def search_inventory(q: str = None):
+    """
+    Search desktop and network inventory by hostname, ip or mac
+    """
     results = {"desktops": [], "network_devices": []}
     if q:
         async with get_session() as session:
