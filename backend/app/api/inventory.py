e1b547b17e164fdd21b1af5e4f2e0b171b140f07
Ryan Bradshaw <ryan@rbradshaw.dev>
2021-11-17 12:10:04 +0000

    tweaks to intergrate with frontend

--- backend/app/api/inventory.py
+++ backend/app/api/inventory.py
@@ -1,6 +1,8 @@
 from operator import or_
 from typing import *
 
+from sqlalchemy.sql.expression import except_
+
 from app.core.inventory.tasks import update_inventory
 from app.core.security.utils import get_current_user
 from app.db import get_engine, get_session
@@ -46,13 +48,22 @@ async def start_desktop_update_task(user: User = Depends(get_current_user)):
 
 @router.get("/network", response_model=List[NetworkReadWithInterfaces], status_code=200)
 async def get_network_devices(
-    site: str = None, device_type: str = None, user: User = Depends(get_current_user)
+    site: str = None,
+    device_type: str = None,
+    nodeids: str = None,
+    user: User = Depends(get_current_user)
 ):
     """
     Filter network devices
     """
     expression = None
-    if site is None and device_type is None:
+    if nodeids:
+        try:
+            nodelist = [int(node) for node in nodeids.split(',')]
+        except ValueError:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="nodeid must be comma separated ids, e.g 100,101,102")
+        expression = select(Network).options(selectinload(Network.interfaces)).where(Network.id.in_(nodelist))
+    elif site is None and device_type is None:
         expression = select(Network).options(selectinload(Network.interfaces))
     elif site and device_type:
         expression = (
@@ -82,12 +93,23 @@ async def get_network_devices(
 
 
 @router.get("/desktop", response_model=List[DesktopReadWithInterface], status_code=200)
-async def get_desktop_devices(site: str = None, user: User = Depends(get_current_user)):
+async def get_desktop_devices(
+    site: str = None,
+    nodeids: str = None,
+    user: User = Depends(get_current_user)):
     """
     Filter desktops
     """
     expression = None
-    if site is None:
+    if nodeids:
+        try:
+            nodelist = [int(node) for node in nodeids.split(',')]
+        except ValueError:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="nodeid must be comma separated ids, e.g 100,101,102")
+        expression = select(Desktop).options(
+            selectinload(Desktop.interface).selectinload(Interface.network_device)
+        ).where(Desktop.id.in_(nodelist))
+    elif site is None:
         expression = select(Desktop).options(
             selectinload(Desktop.interface).selectinload(Interface.network_device)
         )
@@ -164,6 +186,7 @@ async def search_inventory(q: str = None, user: User = Depends(get_current_user)
                 select(Network).where(
                     or_(
                         Network.hostname.ilike(f"%{q}%"),
+                        Network.ip.ilike(f"%{q}%")
                     )
                 )
             ).all()
