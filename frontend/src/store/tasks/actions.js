52f91c4c7df8118439cb3ca0031a6896332baf42
Ryan Bradshaw <ryan@rbradshaw.dev>
2021-11-22 17:48:40 +0000

    added global config for frontend

--- frontend/src/store/tasks/actions.js
+++ frontend/src/store/tasks/actions.js
@@ -1,10 +1,11 @@
 import { api } from 'boot/axios'
+import { config } from 'boot/config'
 
 export function getSiteStatus ({ commit }, site) {
   return api.get(`/tasks/network/site_status/${site}`)
     .then((response) => {
       commit('SET_TASK_ID', response.data.task_id)
-      const ws = new WebSocket(`wss://api.networkdev.co.uk/tasks/ws/${response.data.task_id}`)
+      const ws = new WebSocket(`${config.WS_URL}/${response.data.task_id}`)
       ws.onmessage = (event) => {
         const taskData = JSON.parse(JSON.parse(event.data))
 
@@ -18,7 +19,7 @@ export function getSiteStatus ({ commit }, site) {
 export function activateDeactivateSite ({ dispatch }, { action, site }) {
   return api.post(`/tasks/network/${action.value}_site`, { site: site.value })
     .then((response) => {
-      const ws = new WebSocket(`wss://api.networkdev.co.uk/tasks/ws/${response.data.task_id}`)
+      const ws = new WebSocket(`${config.WS_URL}/${response.data.task_id}`)
       ws.onmessage = (event) => {
         const taskData = JSON.parse(JSON.parse(event.data))
 
