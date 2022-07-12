409ca87a0bc5cf174325838bca10387b5cf6f190
Ryan Bradshaw <ryan@rbradshaw.dev>
2022-07-12 15:03:13 +0100

    working on frontend

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.networkdev.co.uk' })
+const api = axios.create({ baseURL: 'http://localhost/api' })
 // const $router = useRouter()
 
 export default boot(({ app }) => {
